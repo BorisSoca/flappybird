fb8f24f7ac59fbb4b2dc21a05afea7d85ed4e42f
Adrian Chong <agchong3@gmail.com>
2016-07-15 13:20:57 -0400

    did stuff

--- main.js
+++ main.js
@@ -18,11 +18,19 @@ var mainState = {
      spaceKey.onDown.add(this.jump, this);
      this.pipes = game.add.group();
      this.timer = game.time.events.loop(1500, this.addRowOfPipes, this);
+
+     this.score = 0;
+     this.labelScore = game.add.text(20, 20, "0",
+         { font: "30px Arial", fill: "#ffffff" });
+
+
     },
 
     update: function() {
       if (this.bird.y < 0 || this.bird.y > 490)
              this.restartGame();
+      game.physics.arcade.overlap(
+        this.bird, this.pipes, this.restartGame, null,this);
 
     },
 
@@ -62,6 +70,12 @@ var mainState = {
         for (var i = 0; i < 8; i++)
             if (i != hole && i != hole + 1)
                 this.addOnePipe(400, i * 60 + 10);
+
+
+                this.score += 1;
+                this.labelScore.text = this.score;  
+
+
     },
 
 
